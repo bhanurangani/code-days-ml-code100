4fb5478dab23196b1115b6ef952ce4357888d96a
bhanurangani <40607998+bhanurangani@users.noreply.github.com>
2018-07-19 23:37:05 +0530

    codes

---
+++ code/day-16/MongoInsert.py
@@ -0,0 +1,12 @@
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+mydict = { "name": "John", "address": "Highway 37" }
+
+
+x=mycol.find_one()
+
+print(x)
